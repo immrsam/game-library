222bf8de6b5e641ce0d73a09c75a10447c45d86e
Sam <sam.bradbury@hotmail.com>
2024-06-20 15:51:49 +1000

    build colormode switch

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -5,7 +5,7 @@ function ColorModeSwitch() {
 
   return (
     <HStack>
-      <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode} />
+      <Switch colorScheme="red" isChecked={colorMode === "dark"} onChange={toggleColorMode} />
       <Text>Dark Mode</Text>
     </HStack>
   );
