69cb5033d26afa4af65e7396061600b5d07f67e2
Sam <sam.bradbury@hotmail.com>
2024-06-24 11:55:33 +1000

    updated heading and title

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Heading, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
@@ -6,6 +6,7 @@ function NavBar() {
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
+      <Heading>GAME LIBRARY</Heading>
       <ColorModeSwitch />
     </HStack>
   );
